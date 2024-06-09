2d204fa2cc54a0b240048190da968ef877d802af
Siddharth Shah <siddharth.shah@mail.com>
2024-06-09 18:57:21 +0530

    compiler: functions to emit bytecode; functions to compile number tokens

--- compiler.c
+++ compiler.c
@@ -14,6 +14,12 @@ typedef struct
 } Parser;
 
 Parser parser;
+Chunk *compilingChunk;
+
+static Chunk *getCurrentChunk()
+{
+    return compilingChunk;
+}
 
 static void errorAt(Token *token, const char *message)
 {
@@ -75,15 +81,68 @@ static void consume(TokenType type, const char *message)
     errorAtCurrent(message);
 }
 
+// append byte to chunk
+static void emitByte(uint8_t byte)
+{
+    writeChunk(getCurrentChunk(), byte, parser.previous.line);
+}
+
+// append two bytes to chunk. (convenience function made for two byte operations)
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static uint8_t makeConstant(Value value)
+{
+    int constantIdx = addConstant(getCurrentChunk(), value);
+    // since OP_CONSTANT instruction uses one byte to store the index, we can only store upto 256 consts.
+    if (constantIdx > UINT8_MAX)
+    {
+        errorAtPrevious("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constantIdx;
+}
+
+static void emitConstant(Value value)
+{
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void emitReturn()
+{
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler()
+{
+    emitReturn();
+}
+
+static void compileNumberToken()
+{
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void expression()
+{
+}
+
 void compileCode(const char *sourceCode, Chunk *chunk)
 {
     initScanner(sourceCode);
-
     parser.hadError = false;
     parser.panicMode = false;
+
+    compilingChunk = chunk;
 
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
 }
