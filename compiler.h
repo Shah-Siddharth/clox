2ad5ae37865d9795b286e4e21532fa002f003973
Siddharth Shah <siddharth.shah@mail.com>
2024-08-11 15:55:34 +0530

    fix certain function and macro definition bugs

--- compiler.h
+++ compiler.h
@@ -1,9 +1,10 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
+#include "object.h"
 #include "vm.h"
 
 // compile source code and fill the chunk with bytecode
-void compileCode(const char *sourceCode, Chunk *chunk);
+bool compileCode(const char *sourceCode, Chunk *chunk);
 
 #endif
